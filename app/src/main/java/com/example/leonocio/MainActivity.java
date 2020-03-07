8582782cd91bbf693e0609478fdc282e398fa5b7
CmaDam2132020 <61684311+CmaDam2132020@users.noreply.github.com>
2020-03-07 12:28:39 +0100

    Commit de prueba

--- app/src/main/java/com/example/leonocio/MainActivity.java
+++ app/src/main/java/com/example/leonocio/MainActivity.java
@@ -8,7 +8,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        //Commit de prueba
+        //Commit de prueba 2
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
