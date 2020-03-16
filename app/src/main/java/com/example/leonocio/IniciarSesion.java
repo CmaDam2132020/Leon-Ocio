dfa001a10c489a766655b4ebd6360b6888a0481d
CmaDam2132020 <61684311+CmaDam2132020@users.noreply.github.com>
2020-03-16 12:58:37 +0100

    Commit inicial

---
+++ app/src/main/java/com/example/leonocio/IniciarSesion.java
@@ -0,0 +1,13 @@
+package com.example.leonocio;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class IniciarSesion extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.inicio_sesion);
+    }
+
+}
--- app/src/main/java/com/example/leonocio/MainActivity.java
+++ app/src/main/java/com/example/leonocio/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.leonocio;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,8 +13,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button boton_iniciar_sesion = findViewById(R.id.boton_iniciar_sesion);
+        Button boton_acceso_invitado = findViewById(R.id.boton_acceso_invitado);
+        View.OnClickListener listener_boton_iniciar_sesion = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView text_view_main = findViewById(R.id.text_view_main);
+                text_view_main.setText("Boton inciar sesion pulsado");
+            }
+        };
+        boton_iniciar_sesion.setOnClickListener(listener_boton_iniciar_sesion);
 
-
-
+        View.OnClickListener listener_boton_acceso_invitado = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView text_view_main = findViewById(R.id.text_view_main);
+                text_view_main.setText("Boton acceso invitado pulsado");
+            }
+        };
+        boton_acceso_invitado.setOnClickListener(listener_boton_acceso_invitado);
     }
 }
