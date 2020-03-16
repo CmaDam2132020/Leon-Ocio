9ed9191b51a70b6c6d64b4153cb1510b5475f770
CmaDam2132020 <61684311+CmaDam2132020@users.noreply.github.com>
2020-03-16 12:39:41 +0100

    Commit de prueba

--- app/src/main/java/com/example/leonocio/MainActivity.java
+++ app/src/main/java/com/example/leonocio/MainActivity.java
@@ -8,9 +8,10 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        // Mensaje a copiar
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
     }
 }
