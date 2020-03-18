459e4483acdb5c52ca867c1a2f25fbfa71f4d1d5
CmaDam2132020 <61684311+CmaDam2132020@users.noreply.github.com>
2020-03-18 12:08:20 +0100

    Conexion php con argumentos post

--- app/src/main/java/com/example/leonocio/IniciarSesion.java
+++ app/src/main/java/com/example/leonocio/IniciarSesion.java
@@ -4,7 +4,9 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
@@ -14,6 +16,7 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class IniciarSesion extends Activity {
@@ -21,6 +24,8 @@ public class IniciarSesion extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.inicio_sesion);
+        final EditText edit_text_nombre = findViewById(R.id.edit_text_nombre);
+        final EditText edit_text_pass = findViewById(R.id.edit_text_pass);
         Button boton_iniciar_sesion_is = findViewById(R.id.boton_iniciar_sesion_is);
         View.OnClickListener listener_boton_iniciar_sesion_is = new View.OnClickListener() {
             @Override
@@ -29,17 +34,26 @@ public class IniciarSesion extends Activity {
                 StringRequest php_request = new StringRequest(Request.Method.POST, "http://192.168.56.1/leon_ocio/test.php", new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
-                        text_view_iniciar_sesion.setText(response);
+                        if(!response.isEmpty()){
+                            text_view_iniciar_sesion.setText(response);
+
+                        }else{
+                            Toast.makeText(getApplicationContext(),"Usuario o contraseña incorrectos",Toast.LENGTH_SHORT).show();
+                        }
+
                     }
                 }, new Response.ErrorListener() {
                     @Override
                     public void onErrorResponse(VolleyError error) {
-                        text_view_iniciar_sesion.setText("Error al conectarse al fichero php");
+                        Toast.makeText(getApplicationContext(),"Error al conectarse con el fichero php",Toast.LENGTH_SHORT).show();
                     }
                 }){
                     @Override
                     protected Map<String, String> getParams() throws AuthFailureError {
-                        return super.getParams();
+                        Map<String,String> parametros = new HashMap<String,String>();
+                        parametros.put("usuario",edit_text_nombre.getText().toString());
+                        parametros.put("pass",edit_text_pass.getText().toString());
+                        return parametros;
                     }
                 };
 
